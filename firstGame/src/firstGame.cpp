980e1bfa5b14a12d63daf99da1d5ada01a60a7cc
iarwain <iarwain@orx-project.org>
2020-01-02 15:18:38 -0500

    Cleanup pass: - Textures scaled down by 32x32 - Refactored player/enemy movement code (first pass) - AnimationSet refactoring (first pass) - Removed extra camera -> game renders only once per frame now - Removed unnecessary build files from repository - Camera object is now handled entirely in config - Pistol/Rifle are now created separately and linked in config - Data can now be tweaked on the fly with debug build

--- firstGame/src/firstGame.cpp
+++ firstGame/src/firstGame.cpp
@@ -6,7 +6,6 @@
 #include "orx.h"
 
 orxOBJECT* player;
-orxOBJECT* dummy;
 orxOBJECT* spider;
 orxOBJECT* pistol;
 orxOBJECT* rifle;
@@ -14,100 +13,86 @@ orxOBJECT* playersPistol;
 orxOBJECT* playersRifle;
 orxCAMERA* camera;
 bool canJump = true;
-bool isRight = true;
 bool hasPistol = false;
 bool hasRifle = false;
 bool pistolPickUp = false;
 bool riflePickUp = false;
-int moveRight = 10;
 
 void Movement() {
+    orxConfig_PushSection(orxObject_GetName(player));
+    orxFLOAT horizontalSpeed = orxConfig_GetFloat("HorizontalSpeed");
 
-    orxVECTOR leftSpeed = { -19.5, 0, 0 };
-    orxVECTOR rightSpeed = { 19.5, 0, 0 };
+    orxVECTOR speed;
+    orxObject_GetSpeed(player, &speed);
 
-    orxVECTOR flipLeft = { -1, 1, 1 };
-    orxVECTOR flipRight = { 1, 1, 1 };
+    if(orxInput_IsActive("GoLeft"))
+    {
+        speed.fX = -horizontalSpeed;
+        if(hasPistol)
+        {
+            orxObject_SetTargetAnim(player, "playerWalkLeftPistol");
+        }
+        else if (hasRifle)
+        {
+            orxObject_SetTargetAnim(player, "playerWalkLeftRifle");
+        }
+        else
+        {
+            orxObject_SetTargetAnim(player, "playerWalkLeft");
+        }
+    }
+    else if (orxInput_IsActive("GoRight"))
+    {
+        speed.fX = horizontalSpeed;
+        if(hasPistol)
+        {
+            orxObject_SetTargetAnim(player, "playerWalkRightPistol");
+        }
+        else if (hasRifle)
+        {
+            orxObject_SetTargetAnim(player, "playerWalkRightRifle");
+        }
+        else
+        {
+            orxObject_SetTargetAnim(player, "playerWalkRight");
+        }
+    }
+    else
+    {
+        speed.fX = 0.0f;
+        orxObject_SetTargetAnim(player, orxNULL);
+    }
+    orxObject_SetSpeed(player, &speed);
 
-    if (orxInput_IsActive("GoLeft") && hasPistol == false && hasRifle == false)
+    if (orxInput_HasBeenActivated("Jump") && canJump == true)
     {
-        orxObject_ApplyImpulse(player, &leftSpeed, orxNULL);
-        orxObject_SetTargetAnim(player, "playerWalkLeft");
-        isRight = false;
-    }
-    if (orxInput_IsActive("GoRight") && hasPistol == false && hasRifle == false)
-    {
-        orxObject_ApplyImpulse(player, &rightSpeed, orxNULL);
-        orxObject_SetTargetAnim(player, "playerWalkRight");
-        isRight = true;
-    }
-    if (orxInput_IsActive("GoLeft") && hasPistol == true && hasRifle == false)
-    {
-        orxObject_ApplyImpulse(player, &leftSpeed, orxNULL);
-        orxObject_SetTargetAnim(player, "playerWalkLeftPistol");
-        isRight = false;
-
-        orxObject_SetScale(playersPistol, &flipLeft);
-    }
-    if (orxInput_IsActive("GoRight") && hasPistol == true && hasRifle == false)
-    {
-        orxObject_ApplyImpulse(player, &rightSpeed, orxNULL);
-        orxObject_SetTargetAnim(player, "playerWalkRightPistol");
-        isRight = true;
-
-        orxObject_SetScale(playersPistol, &flipRight);
-    }
-    if (orxInput_IsActive("GoLeft") && hasPistol == false && hasRifle == true)
-    {
-        orxObject_ApplyImpulse(player, &leftSpeed, orxNULL);
-        orxObject_SetTargetAnim(player, "playerWalkLeftRifle");
-        isRight = false;
-
-        orxObject_SetScale(playersRifle, &flipLeft);
-    }
-    if (orxInput_IsActive("GoRight") && hasPistol == false && hasRifle == true)
-    {
-        orxObject_ApplyImpulse(player, &rightSpeed, orxNULL);
-        orxObject_SetTargetAnim(player, "playerWalkRightRifle");
-        isRight = true;
-
-        orxObject_SetScale(playersRifle, &flipRight);
-    }
-    else if (isRight == true && hasPistol == false && hasRifle == false)
-    {
-        orxObject_SetTargetAnim(player, "playerIdleRight");
-    }
-    else if (isRight == false && hasPistol == false && hasRifle == false)
-    {
-        orxObject_SetTargetAnim(player, "playerIdleLeft");
-    }
-    else if (isRight == true && hasPistol == true && hasRifle == false)
-    {
-        orxObject_SetTargetAnim(player, "playerIdleRightPistol");
-        orxObject_SetScale(playersPistol, &flipRight);
-    }
-    else if (isRight == false && hasPistol == true && hasRifle == false)
-    {
-        orxObject_SetTargetAnim(player, "playerIdleLeftPistol");
-        orxObject_SetScale(playersPistol, &flipLeft);
-    }
-    else if (isRight == true && hasPistol == false && hasRifle == true)
-    {
-        orxObject_SetTargetAnim(player, "playerIdleRightRifle");
-        orxObject_SetScale(playersRifle, &flipRight);
-    }
-    else if (isRight == false && hasPistol == false && hasRifle == true)
-    {
-        orxObject_SetTargetAnim(player, "playerIdleLeftRifle");
-        orxObject_SetScale(playersRifle, &flipLeft);
-    }
-
-    orxVECTOR jumpSpeed = { 0, -1340 /* -2680 with Jump Pack */, 0 };
-
-    if (orxInput_IsActive("Jump") && orxInput_HasNewStatus("Jump") && canJump == true)
-    {
+        orxVECTOR jumpSpeed;
+        orxConfig_GetVector("JumpSpeed", &jumpSpeed);
         orxObject_ApplyImpulse(player, &jumpSpeed, orxNULL);
         canJump = false;
+    }
+    orxConfig_PopSection();
+}
+
+void EnemyMovement() {
+    for (orxOBJECT* enemy = orxOBJECT(orxStructure_GetFirst(orxSTRUCTURE_ID_OBJECT));
+        enemy;
+        enemy = orxOBJECT(orxStructure_GetNext(enemy)))
+    {
+        orxConfig_PushSection(orxObject_GetName(enemy));
+
+        if(orxConfig_GetBool("IsEnemy"))
+        {
+            orxVECTOR speed;
+            orxObject_GetSpeed(enemy, &speed);
+
+            orxFLOAT horizontalSpeed = orxConfig_GetFloat("HorizontalSpeed");
+
+            // Just going left for now
+            speed.fX = -horizontalSpeed;
+            orxObject_SetSpeed(enemy, &speed);
+        }
+        orxConfig_PopSection();
     }
 }
 
@@ -203,30 +188,18 @@ orxSTATUS orxFASTCALL Init()
 
     player = orxObject_CreateFromConfig("playerObject");
 
-    playersRifle = (orxOBJECT*)orxObject_GetChild(player);
+    playersRifle = orxObject_CreateFromConfig("playersRifle");
 
-    playersPistol = (orxOBJECT*)orxObject_GetSibling(playersRifle);
-
-    dummy = orxObject_CreateFromConfig("dummyObject");
+    playersPistol = orxObject_CreateFromConfig("playersPistol");
 
     orxObject_CreateFromConfig("spaceShipMiddlePlatformObject");
 
-    camera = orxCamera_CreateFromConfig("Camera");
-
     orxEvent_AddHandler(orxEVENT_TYPE_PHYSICS, PhysicsEventHandler);
-
-    orxCamera_SetParent(camera, dummy);
-
-    orxObject_SetParent(dummy, player);
 
     pistol = orxObject_CreateFromConfig("pistolObject");
 
     rifle = orxObject_CreateFromConfig("rifleObject");
 
-    orxObject_Enable(playersPistol, orxFALSE);
-
-    orxObject_Enable(playersRifle, orxFALSE);
-   
     return orxSTATUS_SUCCESS;
 }
 
@@ -247,29 +220,17 @@ void PickUp()
 
 void Shoot()
 {
-    if (orxInput_IsActive("Shoot") && hasPistol == true && hasRifle == false)
-    {
-        orxObject_Enable(playersPistol, orxTRUE);
-    }
-    else {
-        orxObject_Enable(playersPistol, orxFALSE);
-    }
-    if (orxInput_IsActive("Shoot") && hasRifle == true && hasPistol == false)
-    {
-        orxObject_Enable(playersRifle, orxTRUE);
-    }
-    else {
-        orxObject_Enable(playersRifle, orxFALSE);
-    }
+    orxObject_Enable(playersPistol, orxInput_IsActive("Shoot") && hasPistol);
+    orxObject_Enable(playersRifle, orxInput_IsActive("Shoot") && hasRifle);
 }
 
 orxSTATUS orxFASTCALL Run()
 {
     orxSTATUS eResult = orxSTATUS_SUCCESS;
-    
+
     if(orxInput_IsActive("Quit"))
     {
-        
+
         eResult = orxSTATUS_FAILURE;
     }
 
@@ -278,32 +239,34 @@ orxSTATUS orxFASTCALL Run()
     Movement();
 
     Shoot();
-   
+
+    EnemyMovement();
+
     return eResult;
 }
-    
+
 void orxFASTCALL Exit()
 {
-    
+
 }
 
 orxSTATUS orxFASTCALL Bootstrap()
 {
-    
+
     orxResource_AddStorage(orxCONFIG_KZ_RESOURCE_GROUP, "../data/config", orxFALSE);
 
-    
+
     return orxSTATUS_SUCCESS;
 }
 
 int main(int argc, char **argv)
 {
-    
+
     orxConfig_SetBootstrap(Bootstrap);
 
-    
+
     orx_Execute(argc, argv, Init, Run, Exit);
 
-    
+
     return EXIT_SUCCESS;
 }
